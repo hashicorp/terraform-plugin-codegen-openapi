393a69cb378215b14bbedb154f7d1b0604077099
Austin Valle <austinvalle@gmail.com>
2023-08-23 16:31:34 -0400

    remove merge package

--- internal/mapper/merge/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
-// The merge package provides functionality for recursively merging 'n' slices of resource attributes and data source attributes respectively.
-package merge
