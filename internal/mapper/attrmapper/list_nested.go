facb1751c897cae78cadf3708e5ce783c66400cd
Stuart McLaren <stuart.mclaren@hpe.com>
2024-08-15 14:29:19 +0100

    Allow overriding computed_optional_required
    This can be useful, for example, when the person writing
    the Terraform provider does not control the contents of
    the OpenAPI spec.
    
    For example to set an attribute to "required":
    
    ```
            attributes:
              overrides:
                name:
                 description: The new description for name
                 computed_optional_required: required
    ```

--- internal/mapper/attrmapper/list_nested.go
+++ internal/mapper/attrmapper/list_nested.go
@@ -4,11 +4,13 @@
 package attrmapper
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-codegen-openapi/internal/explorer"
 	"github.com/hashicorp/terraform-plugin-codegen-openapi/internal/mapper/util"
 	"github.com/hashicorp/terraform-plugin-codegen-spec/datasource"
 	"github.com/hashicorp/terraform-plugin-codegen-spec/provider"
 	"github.com/hashicorp/terraform-plugin-codegen-spec/resource"
+	"github.com/hashicorp/terraform-plugin-codegen-spec/schema"
 )
 
 type ResourceListNestedAttribute struct {
@@ -39,6 +41,23 @@ func (a *ResourceListNestedAttribute) Merge(mergeAttribute ResourceAttribute) (R
 
 func (a *ResourceListNestedAttribute) ApplyOverride(override explorer.Override) (ResourceAttribute, error) {
 	a.Description = &override.Description
+
+	switch override.ComputedOptionalRequired {
+	case "": // No override
+	case "computed":
+		a.ComputedOptionalRequired = schema.Computed
+	case "optional":
+		a.ComputedOptionalRequired = schema.Optional
+	case "required":
+		a.ComputedOptionalRequired = schema.Required
+	case "computed_optional":
+		a.ComputedOptionalRequired = schema.ComputedOptional
+	default:
+		return nil, fmt.Errorf(
+			"invalid value for computed_optional_required: %s",
+			override.ComputedOptionalRequired,
+		)
+	}
 
 	return a, nil
 }
